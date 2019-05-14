ccee865087761c97cef38f8ff0331cb378e5fa8e
Romeu Carreira da Ponte <romeudaponte@hotmail.com>
2019-05-14 12:23:11 +0100

    First commit

---
+++ src/components/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  estilos = { margin: 0 };
+
+  render() {
+    return <p style={this.estilos}>Log in</p>;
+  }
+}
+
+export default Login;
