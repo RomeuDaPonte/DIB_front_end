683d5c49c50812066d5fe133f6ff3738985b9893
Romeu da Ponte <romeudaponte@gmail.com>
2019-07-04 07:26:24 +0100

    Melhorada implementação da tabela que mostra os preços

--- src/components/definicoes/definicoes.jsx
+++ src/components/definicoes/definicoes.jsx
@@ -40,45 +40,16 @@ class Definicoes extends Component {
             </div>
           </div>
         </div>
-        <div className="card m-2">
-          <h4 className="card-header bg-info text-white bg-dark">
-            Preços em vigor
-          </h4>
-          <div className="card-body">
-            <div className="row">
-              <div className="col">
-                <h1>Automação</h1>
-              </div>
-              <div className="col">
-                <h1>Consultoria</h1>
-              </div>
-              <div className="col">
-                <h1>Desenvolvimento</h1>
-              </div>
-              <div className="col">
-                <h1>Maquinação</h1>
-              </div>
-              <div className="col">
-                <h1>Margem</h1>
-              </div>
-              <div className="col">
-                <h1>Montagem</h1>
-              </div>
-            </div>
-            <div className="row">
-              {this.state.data && (
-                <TabelaDePrecos
-                  automacao={precos.automacao}
-                  consultoria={precos.consultoria}
-                  desenvolvimento={precos.desenvolvimento}
-                  maquinacao={precos.maquinacao}
-                  margem={precos.margem}
-                  montagem={precos.montagem}
-                />
-              )}
-            </div>
-          </div>
-        </div>
+        {this.state.data && (
+          <TabelaDePrecos
+            automacao={precos.automacao}
+            consultoria={precos.consultoria}
+            desenvolvimento={precos.desenvolvimento}
+            maquinacao={precos.maquinacao}
+            margem={precos.margem}
+            montagem={precos.montagem}
+          />
+        )}
       </React.Fragment>
     );
   }
--- src/components/definicoes/tabelaDePrecos.jsx
+++ src/components/definicoes/tabelaDePrecos.jsx
@@ -9,26 +9,53 @@ const TabelaDePrecos = ({
   montagem
 }) => {
   return (
-    <React.Fragment>
-      <div className="col">
-        <h1>{automacao}</h1>
+    <div className="card m-2">
+      <h4 className="card-header bg-info text-white bg-dark">
+        Preços em vigor
+      </h4>
+      <div className="card-body">
+        <div className="row">
+          <div className="col">
+            <h1>Automação</h1>
+          </div>
+          <div className="col">
+            <h1>Consultoria</h1>
+          </div>
+          <div className="col">
+            <h1>Desenvolvimento</h1>
+          </div>
+          <div className="col">
+            <h1>Maquinação</h1>
+          </div>
+          <div className="col">
+            <h1>Margem</h1>
+          </div>
+          <div className="col">
+            <h1>Montagem</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h1>{automacao}</h1>
+          </div>
+          <div className="col">
+            <h1>{consultoria}</h1>
+          </div>
+          <div className="col">
+            <h1>{desenvolvimento}</h1>
+          </div>
+          <div className="col">
+            <h1>{maquinacao}</h1>
+          </div>
+          <div className="col">
+            <h1>{margem} %</h1>
+          </div>
+          <div className="col">
+            <h1>{montagem}</h1>
+          </div>
+        </div>
       </div>
-      <div className="col">
-        <h1>{consultoria}</h1>
-      </div>
-      <div className="col">
-        <h1>{desenvolvimento}</h1>
-      </div>
-      <div className="col">
-        <h1>{maquinacao}</h1>
-      </div>
-      <div className="col">
-        <h1>{margem} %</h1>
-      </div>
-      <div className="col">
-        <h1>{montagem}</h1>
-      </div>
-    </React.Fragment>
+    </div>
   );
 };
 
