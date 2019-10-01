0e7a43f98fc77334b858dbcb57b6683b743de027
Romeu da Ponte <romeudaponte@gmail.com>
2019-10-01 11:22:27 +0100

    landing page reimplementada usando react hooks

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Sidbar from "./sidbar";
 import accountService from "../services/accountService";
 import Definicoes from "./definicoes/definicoes";
 import Entidade from "./entidade/entidade";
 
-class Landing extends Component {
-  state = {
+const Landing = () => {
+  const state = {
     user: null
   };
 
-  componentDidMount() {
-    const user = accountService.getCurrentUser();
-    this.setState({ user });
-  }
+  const [currentState, setState] = useState(state);
 
-  render() {
-    return (
-      <div className="container-fluid p-0">
-        <Sidbar user={this.state.user} />
-        <div className="contentorPrincipal">
-          <Switch>
-            <Route path="/definicoes" component={Definicoes} />
-            <Route path="/entidade" component={Entidade} />
-          </Switch>
-        </div>
+  useEffect(() => {
+    const user = accountService.getCurrentUser();
+    setState({ user });
+  }, []);
+
+  return (
+    <div className="container-fluid p-0">
+      <Sidbar user={currentState.user} />
+      <div className="contentorPrincipal">
+        <Switch>
+          <Route path="/definicoes" component={Definicoes} />
+          <Route path="/entidade" component={Entidade} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Landing;
